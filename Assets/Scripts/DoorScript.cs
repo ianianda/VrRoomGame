82c635e65b43a5233430fb03c799b7a239e7be70
ianianda <ywang402@my.bcit.ca>
2019-01-21 12:12:33 -0800

    added files

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    private Animator _animator = null;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Door Open!");
+
+        _animator.SetBool("isOpen", true);
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        Debug.Log("Door Closed!");
+
+        _animator.SetBool("isOpen", false);
+    }
+}
---
+++ Assets/Scripts/Drawer1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer1 : MonoBehaviour
+{
+
+    private Animator _animator = null;
+    private bool drawer1Open;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        drawer1Open = false;
+    }
+
+    void OnMouseDown()
+    {
+        if (drawer1Open == true)
+        {
+            Debug.Log("Drawer 1 Open!");
+
+            _animator.SetBool("DrawerIsOpen", true);
+            drawer1Open = false;
+        }
+        else
+        {
+            Debug.Log("Drawer 1 Closed!");
+
+            _animator.SetBool("DrawerIsOpen", false);
+            drawer1Open = true;
+        }
+    }
+}
